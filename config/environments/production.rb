68ca94f894a04a8aad1547770352720e78ad2fea
Sho Sugahara <showgm123@gmail.com>
2018-11-24 12:35:12 +0900

    再コミット

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,10 +92,10 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
+    :address => "*******",
     :port => 587,
-    :user_name => "***********",
-    :password => "************",
+    :user_name => "*******",
+    :password => "********",
     :authentication => :plain,
     :enable_starttls_auto => true
 
